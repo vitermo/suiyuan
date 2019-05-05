89bb499f9326ea58d3c9d00d49b9b3bb4f5acd62
vitermo <vitermo@163.com>
2019-05-05 17:20:57 +0800

    settings debug=true

--- suiyuan/settings.py
+++ suiyuan/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'debw0306d0lx$l!58*fp+091+435)kkr637x)bpbp^^ttgyxez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','120.78.157.87']
 
